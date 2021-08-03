a8ae96e1bce78b5c4bf2456512fd8dcfa41bbc30
Kwabena W. Agyeman <kwagyeman@live.com>
2021-08-02 23:21:01 -0700

    Fix Arduino driver install

--- share/qtcreator/drivers/arduino/arduino.cmd
+++ share/qtcreator/drivers/arduino/arduino.cmd
@@ -4,18 +4,18 @@ if DEFINED PROCESSOR_ARCHITEW6432 goto :install_wow
 
 :install
 if DEFINED PROCESSOR_ARCHITECTURE goto :install_64
-"%~dp0\dpinst_x86.exe" /se /sw
+"%~dp0\dpinst-x86.exe" /se /sw
 exit
 
 :install_64
 if %PROCESSOR_ARCHITECTURE%==x86 goto :install_32
-"%~dp0\dpinst_amd64.exe" /se /sw
+"%~dp0\dpinst-amd64.exe" /se /sw
 exit
 
 :install_32
-"%~dp0\dpinst_x86.exe" /se /sw
+"%~dp0\dpinst-x86.exe" /se /sw
 exit
 
 :install_wow
-"%~dp0\dpinst_amd64.exe" /se /sw
+"%~dp0\dpinst-amd64.exe" /se /sw
 exit
