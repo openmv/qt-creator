e130988fb9ec0703e08f074bbb938b615217f5b4
Kwabena W. Agyeman <kwagyeman@live.com>
2023-06-06 14:14:43 -0700

    Cleanup document tab bar look

--- src/plugins/texteditor/displaysettings.h
+++ src/plugins/texteditor/displaysettings.h
@@ -52,7 +52,11 @@ public:
     bool m_openLinksInNextSplit = false;
     bool m_forceOpenLinksInNextSplit = false;
     bool m_displayFileEncoding = false;
-    bool m_displayFileLineEnding = true;
+    // OPENMV-DIFF //
+    // bool m_displayFileLineEnding = true;
+    // OPENMV-DIFF //
+    bool m_displayFileLineEnding = false;
+    // OPENMV-DIFF //
     bool m_scrollBarHighlights = true;
     bool m_animateNavigationWithinFile = false;
     int m_animateWithinFileTimeMax = 333; // read only setting
