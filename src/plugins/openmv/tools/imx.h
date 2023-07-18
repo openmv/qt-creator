9513f6475e62691149474f6a16aabccf59085291
Kwabena W. Agyeman <kwagyeman@live.com>
2023-07-18 14:39:42 -0700

    Add wanrings for raspberrypi

--- src/plugins/openmv/tools/imx.h
+++ src/plugins/openmv/tools/imx.h
@@ -12,6 +12,7 @@ QList<QPair<int, int> > imxVidPidList(bool spd_host = true, bool bl_host = true)
 // Returns PID/VID of SPD and BL bootloaders on the system.
 QStringList imxGetAllDevices(bool spd_host = true, bool bl_host = true);
 // Returns true if the BL bootloader is present.
+bool imxGetDeviceSupported();
 bool imxGetDevice(QJsonObject &obj);
 bool imxDownloadBootloaderAndFirmware(QJsonObject &obj, bool forceFlashFSErase, bool justEraseFlashFs);
 bool imxDownloadFirmware(QJsonObject &obj, bool forceFlashFSErase, bool justEraseFlashFs);
