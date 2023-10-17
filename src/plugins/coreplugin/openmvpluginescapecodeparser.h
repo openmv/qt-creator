e56781c2129e9a2580a87ed2ce5593998d85c298
Kwabena W. Agyeman <kwagyeman@live.com>
2023-10-17 15:17:53 -0700

    Add documentation for protocol

--- src/plugins/coreplugin/openmvpluginescapecodeparser.h
+++ src/plugins/coreplugin/openmvpluginescapecodeparser.h
@@ -6,16 +6,40 @@
 #include <QtCore>
 #include <QtWidgets>
 
+/* Escape Code Protocol:
+ *
+ * You can trigger OpenMV IDE to do something by sending an escape code
+ * starting with "\x1b[" followed by a sequence of positive integer numbers
+ * seperated by ';' and terminated by 'O'. E.g. "\x1b[1O" or "\x1b[1;1O".
+ * The above is the standard format for escape sequences so that they will
+ * be handled gracefully (and discarded) by any other software than OpenMV IDE.
+ *
+ * Next, the numbers passed represent a function to be called (see enum below)
+ * and any remaining numbers in the list are passed as arguments to said function.
+ *
+ * To send text to OpenMV IDE the START_TEXT escape code '1' is used to redirect all
+ * text that is received after it to a buffer which will be processed when any other
+ * escape code is received. E.g. "\x1b[1OHello World!\x1b[32O" redirects "Hello World!"
+ * from being sent to the terminal to an internal buffer and the DIALOG_INFO escape code
+ * '32' displays the contents of that buffer in a dialog box and turns off text redirection.
+ *
+ * Text is processed differently by each function. So, it's possible to send more complex
+ * arguments (like json) via the text mechanism as the argument for more complex functions.
+ *
+ * Note, text arguments will appear in the standard terminal stream when parsed by other
+ * software than OpenMV IDE. So, the text arguments should be kept readable.
+ */
+
 enum {
-    ESCAPE_CODE_FUNCTION_RESET_SYSTEM = 0,
-    ESCAPE_CODE_FUNCTION_START_TEXT = 1,
+    ESCAPE_CODE_FUNCTION_RESET_SYSTEM = 0, // no args
+    ESCAPE_CODE_FUNCTION_START_TEXT = 1, // no args
 
-    ESCAPE_CODE_FUNCTION_DIALOG_INFO = 32,
-    ESCAPE_CODE_FUNCTION_DIALOG_WARNING = 33,
-    ESCAPE_CODE_FUNCTION_DIALOG_ERROR = 34,
-    ESCAPE_CODE_FUNCTION_DIALOG_QUESTION = 35,
+    ESCAPE_CODE_FUNCTION_DIALOG_INFO = 32, // no args
+    ESCAPE_CODE_FUNCTION_DIALOG_WARNING = 33, // no args
+    ESCAPE_CODE_FUNCTION_DIALOG_ERROR = 34, // no args
+    ESCAPE_CODE_FUNCTION_DIALOG_QUESTION = 35, // no args
 
-    ESCAPE_CODE_FUNCTION_DATASET_EDITOR_SAVE_IMAGE = 64,
+    ESCAPE_CODE_FUNCTION_DATASET_EDITOR_SAVE_IMAGE = 64, // no args
 };
 
 namespace Core {
