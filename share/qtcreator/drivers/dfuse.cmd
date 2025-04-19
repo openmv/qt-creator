62e869c0be6b8efe923a62030029c51b5aa4cf9a
Kwabena W. Agyeman <kwagyeman@live.com>
2025-04-19 14:00:00 -0700

    drivers: Harden dfuse and vcr.

--- share/qtcreator/drivers/dfuse.cmd
+++ share/qtcreator/drivers/dfuse.cmd
@@ -18,58 +18,58 @@ goto :install_10
 :install_7
 if DEFINED PROCESSOR_ARCHITECTURE goto :install_7_64
 "%~dp0\..\dfuse\Driver\Win7\x86\dpinst_x86.exe" /se /sw
-exit
+exit /b 0
 
 :install_7_64
 if %PROCESSOR_ARCHITECTURE%==x86 goto :install_7_32
 "%~dp0\..\dfuse\Driver\Win7\x64\dpinst_amd64.exe" /se /sw
-exit
+exit /b 0
 
 :install_7_32
 "%~dp0\..\dfuse\Driver\Win7\x86\dpinst_x86.exe" /se /sw
-exit
+exit /b 0
 
 :install_8
 if DEFINED PROCESSOR_ARCHITECTURE goto :install_8_64
 "%~dp0\..\dfuse\Driver\Win8\x86\dpinst_x86.exe" /se /sw
-exit
+exit /b 0
 
 :install_8_64
 if %PROCESSOR_ARCHITECTURE%==x86 goto :install_8_32
 "%~dp0\..\dfuse\Driver\Win8\x64\dpinst_amd64.exe" /se /sw
-exit
+exit /b 0
 
 :install_8_32
 "%~dp0\..\dfuse\Driver\Win8\x86\dpinst_x86.exe" /se /sw
-exit
+exit /b 0
 
 :install_8_1
 if DEFINED PROCESSOR_ARCHITECTURE goto :install_8_1_64
 "%~dp0\..\dfuse\Driver\Win8.1\x86\dpinst_x86.exe" /se /sw
-exit
+exit /b 0
 
 :install_8_1_64
 if %PROCESSOR_ARCHITECTURE%==x86 goto :install_8_1_32
 "%~dp0\..\dfuse\Driver\Win8.1\x64\dpinst_amd64.exe" /se /sw
-exit
+exit /b 0
 
 :install_8_1_32
 "%~dp0\..\dfuse\Driver\Win8.1\x86\dpinst_x86.exe" /se /sw
-exit
+exit /b 0
 
 :install_10
 if DEFINED PROCESSOR_ARCHITECTURE goto :install_10_64
 "%~dp0\..\dfuse\Driver\Win10\dpinst_x86.exe" /se /sw
-exit
+exit /b 0
 
 :install_10_64
 if %PROCESSOR_ARCHITECTURE%==x86 goto :install_10_32
 "%~dp0\..\dfuse\Driver\Win10\dpinst_amd64.exe" /se /sw
-exit
+exit /b 0
 
 :install_10_32
 "%~dp0\..\dfuse\Driver\Win10\dpinst_x86.exe" /se /sw
-exit
+exit /b 0
 
 :install_wow
 ver | findstr /c:"Version 5." > nul
@@ -86,16 +86,16 @@ goto :install_wow_10
 
 :install_wow_7
 "%~dp0\..\dfuse\Driver\Win7\x64\dpinst_amd64.exe" /se /sw
-exit
+exit /b 0
 
 :install_wow_8
 "%~dp0\..\dfuse\Driver\Win8\x64\dpinst_amd64.exe" /se /sw
-exit
+exit /b 0
 
 :install_wow_8_1
 "%~dp0\..\dfuse\Driver\Win8.1\x64\dpinst_amd64.exe" /se /sw
-exit
+exit /b 0
 
 :install_wow_10
 "%~dp0\..\dfuse\Driver\Win10\dpinst_amd64.exe" /se /sw
-exit
+exit /b 0
