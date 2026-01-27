490e9c4b08c696e58527fe570789c8a0d593d418
Kwabena W. Agyeman <kwagyeman@live.com>
2026-01-27 14:51:27 -0800

    scripts: Skip signing ST libs.

--- scripts/sign.py
+++ scripts/sign.py
@@ -124,6 +124,10 @@ def signFile(file, args):
         else: print("Skipping")
         return
     elif sys.platform == "darwin":
+        # Skip signing ST Stuff...
+        if "stedgeai" in file:
+            print("Skipping")
+            return
         if codsignAvailable:
             if not os.system("codesign -d --verbose=4 " + file.replace(" ", "\\ ") + " 2>&1 | grep Timestamp > /dev/null") and \
             not os.system("codesign -d --verbose=4 " + file.replace(" ", "\\ ") + " 2>&1 | grep Authority > /dev/null") and \
